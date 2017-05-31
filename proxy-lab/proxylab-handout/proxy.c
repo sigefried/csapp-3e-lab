06fe4bc5c343e004b60fb6a3ae59577bedd988e3
sigefried <sigefriedhyy@gmail.com>
2017-05-31 21:57:46 +0900

    add some init code to proxy.c and re-format csapp.c

--- proxy-lab/proxylab-handout/proxy.c
+++ proxy-lab/proxylab-handout/proxy.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "csapp.h"
 
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
@@ -6,9 +7,41 @@
 
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
+static const char *accept_hdr = "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n";
+static const char *accept_encoding_hdr = "Accept-Encoding: gzip, deflate\r\n";
+static const char *connection_hdr = "Connection: close\r\n";
+static const char *proxy_connection_hdr = "Proxy-Connection: close\r\n";
+
+//funtions definitaion
+void client_error(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg);
 
 int main()
 {
-    printf("%s", user_agent_hdr);
-    return 0;
+	printf("%s", user_agent_hdr);
+	return 0;
+}
+
+
+/*
+ * Build a simple website for cannot connect to server errors
+ */
+void client_error(int fd, char *cause, char *errnum,
+		char *shortmsg, char *longmsg) {
+	char buf[MAXLINE], body[MAXLINE];
+
+	/* Build the HTTP response body */
+	sprintf(body, "<html><title>Request Error</title>");
+	sprintf(body, "%s<body bgcolor=""ffffff"">\r\n", body);
+	sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
+	sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
+	sprintf(body, "%s<hr><em>The proxy</em>\r\n", body);
+
+	/* Print the HTTP response */
+	sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+	Rio_writen(fd, buf, strlen(buf));
+	sprintf(buf, "Content-type: text/html\r\n");
+	Rio_writen(fd, buf, strlen(buf));
+	sprintf(buf, "Content-length: %d\r\n\r\n", (int)strlen(body));
+	Rio_writen(fd, buf, strlen(buf));
+	Rio_writen(fd, body, strlen(body));
 }
